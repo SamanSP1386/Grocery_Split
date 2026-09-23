9dba5999343e9342fe114b7cee451ef2f9695026
Tyler Place <79611123+1withspaghetti@users.noreply.github.com>
2026-09-23 10:54:36 -0700

    feat: added shadcn/ui + tailwind

--- packages/react-frontend/vite.config.ts
+++ packages/react-frontend/vite.config.ts
@@ -1,8 +1,15 @@
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), babel({ presets: [reactCompilerPreset()] })]
+	plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
+	resolve: {
+		alias: {
+			"@": path.resolve(import.meta.dirname, "./src")
+		}
+	}
 });
