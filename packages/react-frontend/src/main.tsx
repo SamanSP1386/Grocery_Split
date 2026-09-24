5972f6f212e51cd0074425fd294ceb24b16ff34c
Tyler Place <79611123+1withspaghetti@users.noreply.github.com>
2026-09-24 15:42:49 -0700

    feat: added react-router in data mode

--- packages/react-frontend/src/main.tsx
+++ packages/react-frontend/src/main.tsx
@@ -1,12 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import "./index.css";
-import App from "./App.tsx";
-import { Button } from "@/components/ui/button.tsx";
+import Index from "./routes/Index";
+
+// https://reactrouter.com/start/data/routing
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Index />
+	}
+]);
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
-		<Button>test</Button>
+		<RouterProvider router={router} />,
 	</StrictMode>
 );
