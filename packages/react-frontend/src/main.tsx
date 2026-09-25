1a77bd12410d0913a5a83835de5530f613db15b0
YushaRencuzogullari <yrencuz1@gmail.com>
2026-09-25 06:46:13 -0700

    Set up React PWA

--- packages/react-frontend/src/main.tsx
+++ packages/react-frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import { registerSW } from "virtual:pwa-register";
 import "./index.css";
 import Index from "./routes/Index";
 
@@ -18,3 +19,12 @@ createRoot(document.getElementById("root")!).render(
 		<RouterProvider router={router} />,
 	</StrictMode>
 );
+
+// prompts for a refresh
+const updateSW = registerSW({
+	onNeedRefresh() {
+		if (confirm("New content available. Reload?")) {
+			updateSW(true);
+		}
+	}
+});
--- packages/react-frontend/vite.config.ts
+++ packages/react-frontend/vite.config.ts
@@ -3,10 +3,40 @@ import babel from "@rolldown/plugin-babel";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
+	plugins: [
+		tailwindcss(),
+		react(),
+		babel({ presets: [reactCompilerPreset()] }),
+		VitePWA({
+			// caches all the imports
+			workbox: {
+				globPatterns: ["**/*"]
+			},
+			// caches all the static assets in the public folder
+			includeAssets: ["**/*"],
+			manifest: {
+				theme_color: "#f69435",
+				background_color: "#f69435",
+				display: "standalone",
+				scope: "/",
+				start_url: "/",
+				short_name: "Grocery Split",
+				description: "Grocery Split",
+				name: "Grocery Split",
+				icons: [
+					{
+						src: "/placeholder.svg",
+						sizes: "1x1",
+						type: "image/svg"
+					}
+				]
+			}
+		})
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(import.meta.dirname, "./src")
